868326d4a551d02135af4411a7ff2e8272e44316
andy <andy@andys-iMac.local>
2023-07-22 13:17:58 +0100

    added notes

--- main.tf
+++ main.tf
@@ -12,13 +12,22 @@ resource "azurerm_api_management" "main" {
   resource_group_name = var.rg_name
   publisher_name      = var.publisher_name
   publisher_email     = var.publisher_email
-  tags                = var.tags
+  
+  # sku_name consists of 2 parts. First part is the name (consumption, premium etc), 2nd is the capacity (No of units).
+  # Consumption plans are autoscaling, so always should be `0`.. All other Sku names must be a positive number.
+  # Developer plans can only have 1 unit deployed.
   sku_name            = "${var.sku}_${(var.sku == "Consumption") ? 0 : ((var.sku == "Developer") ? 1 : var.skuCount)}"
+  tags                = var.tags
   
   identity  {
     type = "SystemAssigned"
   }
+
 }
+
+
+
+
 
 # Create Logger
 resource "azurerm_api_management_logger" "main" {
